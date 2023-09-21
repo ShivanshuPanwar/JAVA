2d4a83ca31e4b7de6385593109219dfbf806cab3
Shivanshu Panwar <118122773+ShivanshuPanwar@users.noreply.github.com>
2023-09-21 18:45:24 +0530

    Update function2.java

--- Loops & Method/function2.java
+++ Loops & Method/function2.java
@@ -1,6 +1,14 @@
-//function with parameters
-
+// Print a given name in a function
+import java.util.*;
 public class function2 {
-    
 
+    public static void printMyName(String name) {
+     System.out.println(name);
+     return ;
+}
+    public static void main(String args[]) {
+           Scanner sc = new Scanner(System.in);
+           String name = sc.next();
+           System.out.println(name);
+    }
 }
